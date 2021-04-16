135f4aca26407aeb922d8d8493f86e4e4cf8d909
Petr Rajtslegr <petr.rajtslegr@gmail.com>
2021-04-16 20:41:58 +0200

    fetcher fix

--- src/pages/utils/fetcher.ts
+++ src/pages/utils/fetcher.ts
@@ -1,8 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-export const fetcher = async <JSON = any>(
-  input: RequestInfo,
-  init?: RequestInit,
-): Promise<JSON> => {
+export default async <JSON = any>(input: RequestInfo, init?: RequestInit): Promise<JSON> => {
   const res = await fetch(input, init);
   return res.json();
 };
